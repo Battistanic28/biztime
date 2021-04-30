c67f7bee4486b38423b8bdeb86bbf6affd3dc192
Nick Battista <nickbattista@Nick.Battista>
2021-04-30 13:00:43 -0600

    added slugify

--- routes/companies.js
+++ routes/companies.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const slugify = require("slugify");
 const router = new express.Router();
 const ExpressError = require("../expressError");
 const db = require("../db");
@@ -27,7 +28,8 @@ router.get("/:code", async function(req, res, next) {
 
 router.post("/", async function(req, res, next) {
     try {
-        const { code, name, description } = req.body;
+        const { name, description } = req.body;
+        const code = slugify(name, {remove: /[*+~.()'"!:@]/g, lower: true})
         const result = await db.query("INSERT INTO companies (code, name, description) VALUES ($1,$2,$3) RETURNING code, name, description", [code, name, description])
         return res.status(201).json({company: result.rows[0]})
     } catch(err) {
